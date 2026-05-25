8b87a22fdca5a222b18922e277a9e4b353fc3881
airishshaneangub21 <airishshaneangub@gmail.com>
2026-05-25 22:30:28 +0800

    Initial commit

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const path = require('path');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,17 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false,
-    dialectOptions: process.env.DB_DIALECT === 'mysql' && process.env.DB_SSL === 'true' ? {
+    dialectOptions: {
       ssl: {
         rejectUnauthorized: false
       }
-    } : {},
-    storage: process.env.DB_DIALECT === 'sqlite' 
-      ? path.join(__dirname, process.env.DB_NAME || 'database.sqlite') 
-      : null
+    }
   }
 );
 
